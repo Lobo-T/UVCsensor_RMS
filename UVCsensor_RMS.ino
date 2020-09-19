a592a2af936e795b87f07fe31e123e14a84b8f41
Lobo-T <Lobo-T@users.noreply.github.com>
2020-09-19 02:01:03 +0200

    Add files via upload

--- UVCsensor_RMS.ino
+++ UVCsensor_RMS.ino
@@ -93,7 +93,7 @@ void loop() {
   //Send data til DAC
   byte hibyte, lobyte;
   lobyte = sensorRMS << 2;
-  hibyte = sensorRMS >> 6;
+  hibyte = (sensorRMS >> 6) && 00111111;
   Wire.beginTransmission(DACaddr);
   Wire.write(hibyte);
   Wire.write(lobyte);
